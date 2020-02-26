33070527f3721aeab75c2f0d7e61812a0eca2ce1
LucyGTH <tianhangguo@gmail.com>
2020-02-25 20:40:06 -0800

    thirdpartyAPI lab

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,13 @@
 function initMap() {
-	// add your code here
+	L.mapquest.key = 'ofapHPE9XGyA8omD4o5BFZnP4aPK1l7l';
+
+	// 'map' refers to a <div> element with the ID map
+	var map = L.mapquest.map('map', {
+	center: [32.8788,-117.2359],
+	layers: L.mapquest.tileLayer('map'),
+	zoom: 12,
+	zoomControl: false
+	});
+	
+	L.marker([32.8788,-117.2359]).addTo(map);
 }
